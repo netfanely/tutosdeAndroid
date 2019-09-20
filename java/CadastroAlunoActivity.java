1d64ee5996ceb8c0e6ecd9f0704dbc541229f29c
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-09-19 22:23:27 -0500

    Update CadastroAlunoActivity.java

--- java/CadastroAlunoActivity.java
+++ java/CadastroAlunoActivity.java
@@ -1,1 +1,27 @@
 /* Cadastro Aluno Activity */
+package com.netfanely.exemplocrud;
+
+/*import*/
+
+public class CadastroAlunoActivity extends AppCompatActivity{
+  
+    private EditText nome;
+    private EditText cpf;
+    private EditText telefone;
+  
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+          super.onCreate(savedInstanceState);
+          setContentView(R.layout.activity_cadastro_aluno);
+        
+          nome = findViewById(R.id.editNome);
+          cpf = findViewById(R.id.editCPF);
+          telefone = findViewById(R.id.editTelefone);
+    }
+    public void salvar(View view){
+          Aluno a = new Aluno();
+          a.setNome(nome.getText().toString());
+          a.setCpf(cpf.getText().toString());
+          a.setTelefone(telefone.getText().toString());
+    }
+}
