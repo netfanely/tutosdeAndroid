9fde8bb4c8ba9cf5b183080294391c4b928f3619
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-09-19 21:49:37 -0500

    Update ListarAlunosActivity.java

--- java/ListarAlunosActivity.java
+++ java/ListarAlunosActivity.java
@@ -1,1 +1,21 @@
 /*listar alunos*/
+package com.netfanely.exemplocrud;
+
+/*import*/
+
+public class ListarAlunosActivity extends AppCompatActivity {
+
+  private ListView listView;
+  private AlunoDAO dao;
+  private List<Aluno> alunos;
+  private List<Aluno> alunosFiltrados = new ArrayList<>();
+  
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(savedInstanceState);
+      setContentView(R.layout.activity_listar_alunos);
+    
+      listView = findViewById(R.id.lista_alunos);
+      dao = new AlunoDAO(this);
+  }
+}
