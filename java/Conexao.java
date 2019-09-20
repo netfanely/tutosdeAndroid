c504becb273ae50df8057b8719bc9ce453369279
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-09-19 22:34:20 -0500

    Update Conexao.java
    conexao de banco de dados

--- java/Conexao.java
+++ java/Conexao.java
@@ -1,1 +1,19 @@
 /* Conexao */
+public class Conexao extends SQLiteOpenHelper {
+
+  private static final String name="banco.db";
+  private static final int version = 1;
+  
+  public Conexao(Context context, String name, SQLiteDatabase){
+         super(context, name, null, version);
+  }
+  @Override
+  public void onCreate(SQLiteDatabase db){
+          db.execSQL("create table aluno(id integer primary key autoincrement, "+
+                    "nome varchar(50),cpf varchar(50),telefone varchar(50))");
+  }
+  @Override
+  public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i,){
+  
+  }
+}
