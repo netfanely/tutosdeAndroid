58a035e60bcd42e812c59388ea4344834e95fb2c
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-09-19 22:41:55 -0500

    Update CadastroAlunoActivity.java

--- java/CadastroAlunoActivity.java
+++ java/CadastroAlunoActivity.java
@@ -8,6 +8,7 @@ public class CadastroAlunoActivity extends AppCompatActivity{
     private EditText nome;
     private EditText cpf;
     private EditText telefone;
+    private AlunoDAO dao;
   
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -17,11 +18,14 @@ public class CadastroAlunoActivity extends AppCompatActivity{
           nome = findViewById(R.id.editNome);
           cpf = findViewById(R.id.editCPF);
           telefone = findViewById(R.id.editTelefone);
+          dao = new AlunoDAO(this);
     }
     public void salvar(View view){
           Aluno a = new Aluno();
           a.setNome(nome.getText().toString());
           a.setCpf(cpf.getText().toString());
           a.setTelefone(telefone.getText().toString());
+          long id = dao.inserir(a);
+          Toast.makeText(this,"Aluno inserido com id: " + id,Toast.LENGTH_SHORT).show();
     }
 }
