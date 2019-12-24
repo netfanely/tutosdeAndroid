0e5b60af5fda222dd7dc687fab8f714d039cb629
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-12-24 05:55:06 -0500

    Update MainActivity.java
    CHANGE COLOR WITH SENSORMANAGER

--- sensores/MainActivity.java
+++ sensores/MainActivity.java
@@ -1,16 +1,28 @@
+import android.hardware.Sensor;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+   LinearLayout ln;
+   SensorManager sm;
+   Sensor sensor;
+   TextView tv;
 
    @Override
    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+      
+        ln = (LinearLayout) findViewById(R.id.linear);
+        tv = (TextView) findViewById(R.id.texto);
+        sm = (SensorManager)getSystemService(SENSOR_SERVICE);
+        sensor = sm.getDefaultSensor(Sensor.TYPE_PROXIMITY);
    }
    
    @Override
@@ -27,5 +39,24 @@ public class MainActivity extends AppCompatActivity {
            return true;
         }
         return super.onOptionsItemSelected(item);
+   }
+   
+   @Override
+   public void onSensorChanged(SensorEvent event){
+      String texto = String.valueOf(event.values[0]);
+      tv.setText(texto);
+       
+      float valor = Float.parseFloat(texto);
+      
+      if (valor == 0) {
+         ln.setBackgroundColor(Color.BLUE);
+      }else{
+         ln.setBackgroundColor(Color.YELLOW);
+      }
+   }   
+   
+   @Override 
+   public void onAccuracyChanged(Sensor sensor, int accuracy){
+   
    }
 }
