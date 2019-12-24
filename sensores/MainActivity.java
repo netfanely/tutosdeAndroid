e11cf1f64c9cd025b882c623b392f0595b5ea96f
Fanny Fanely <40525222+netfanely@users.noreply.github.com>
2019-12-24 05:41:52 -0500

    Create MainActivity.java
    main activity sensor proximidade

---
+++ sensores/MainActivity.java
@@ -0,0 +1,31 @@
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.LinearLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+
+   @Override
+   protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+   }
+   
+   @Override
+   public boolean onCreateOptionsMenu(Menu Menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+   }
+   
+   @Override
+   public boolean onOptionItemSelected(MenuItem item){
+        int id = item.getItemId();
+        
+        if(id == R.id.action_settings) {
+           return true;
+        }
+        return super.onOptionsItemSelected(item);
+   }
+}
